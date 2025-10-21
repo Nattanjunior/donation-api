edd730b42dc659c2777e013df7b165b7d93bf39f
nattanjunior <fnatanieljunior@gmail.com>
2025-10-21 15:04:34 -0300

    Refactor AuthService to improve user registration and login logic; added validation schemas and enhanced error handling.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from './dtos/createUser-dto';
 import { z } from 'zod'
 import * as bcrypt from 'bcrypt'
-
-
-
+import type { LoginUserDto } from './dtos/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,16 +14,45 @@ export class AuthService {
     readonly jwtService: JwtService,
   ) { }
 
-  async registerUser(user: CreateUserDto) {
+  async registerUser(createUserDto: CreateUserDto) {
     const userSchema = z.object({
       name: z.string(),
-      email: z.string().email(), 
+      email: z.string().email(),
       password: z.string().min(4),
     })
 
-    const { name, email, password } = userSchema.parse(user)
+    const { name, email, password } = userSchema.parse(createUserDto)
     const hashedPassword = await bcrypt.hash(password, 10)
-    const user
+    const createdUser = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      },
+    })
+    return { message: "User created successfully", createdUser }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const userSchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(4),
+    })
+
+    const { email, password } = userSchema.parse(loginUserDto)
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { email }
+    })
+
+    if (!user) {
+      throw new UnauthorizedException("Invalid credentials")
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      throw new UnauthorizedException("Invalid credentials")
+    }
+    return { message: "User logged in successfully", user }
   }
 
 }
