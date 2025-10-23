5bd1ffcf55d5c388a8c8d83311831a74d1c29453
nattanjunior <fnatanieljunior@gmail.com>
2025-10-23 16:58:14 -0300

    feat(donates): add DonatesModule to the application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { DonatesModule } from './donates/donates.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     AuthModule,
     PrismaModule,
     UserModule,
+    DonatesModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService],
