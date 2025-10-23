016345fe301a8ee0dfd28e509a038bdb913f7197
nattanjunior <fnatanieljunior@gmail.com>
2025-10-23 17:00:12 -0300

    style(auth, dtos): format code and add missing newline in GoogleStrategy and CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsEmail, IsOptional, IsBoolean, IsEnum } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsBoolean,
+  IsEnum,
+} from 'class-validator';
 import { Roles } from '@prisma/client';
 
 export class CreateUserDto {
